c92208fd79cd218a1c4cfc32d6b231fdd567b616
KseniyaBodrova <ksyenia.bodrova@gmail.com>
2025-01-21 12:49:48 +0300

    lesson 4

--- lesson 4/My_basics2.py
+++ lesson 4/My_basics2.py
@@ -1,3 +1,16 @@
-a = 1
-a += 2
-print(a)
+# a = 1
+# a += 2
+# print(a)
+# print(a)
+# a = 256
+# b = 256
+# print(a is b)
+#
+# a = 259
+# b = 259
+# print(id(a))
+# print(id(b))
+# print(a is b)
+
+user_id = int(input("id"))
+print(type(user_id), user_id + 2)
